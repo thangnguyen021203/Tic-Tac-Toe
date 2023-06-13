7a7e75c99d4b7deb106f334e71e5f5de1ec94838
Thanggggg <nguyenquocthang0212@gmail.com>
2023-06-14 00:34:50 +0700

    Not implement exploration rate yet.

--- TicTacToe_qlearning.py
+++ TicTacToe_qlearning.py
@@ -4,8 +4,14 @@ class TicTacToe:
     def __init__(self):
         self.board = None
         self.current_state = None
-        c_learning_rate = 0.1
-        c_discount_value = 0.9
+        self.c_learning_rate = 0.1
+        self.c_discount_value = 0.9
+        self.computer = None
+        self.computer_q_table = None
+        self.max_position_to_choose = None
+        self.max_q_value_to_choose = None
+        self.q_table_player_O = None
+        self.q_table_player_X = None
         
     def make_environment(self):
         self.board = np.array([['-','-','-'],
@@ -34,7 +40,7 @@ class TicTacToe:
         self.board = np.array([['-','-','-'],
                               ['-','-','-'],
                               ['-','-','-']], dtype = np.str0)
-        self.current_state = 19628 #Trạng thái bảng rỗng
+        self.current_state = self.convert_to_state() #Trạng thái bảng rỗng
     
     def is_winner(self, player):
         #Check row
@@ -79,9 +85,10 @@ class TicTacToe:
     
     def is_draw(self):
         draw = True
-        for i in self.board:
-            if i != '-':
-                draw = False
+        for i in range(3):
+            for j in (range(3)):
+                if self.board[i][j] == '-':
+                    draw = False
         return draw
     
     def get_reward_player_X(self):
@@ -96,20 +103,33 @@ class TicTacToe:
     
     def get_reward(self, reward_X, reward_O):
         reward_X = self.get_reward_player_X()
-        reward_Y = self.get_reward_player_O()
+        reward_O = self.get_reward_player_O()
         
     def swap_player(self, player):
         return 'X' if player == 'O' else 'O'
     
+    def chooseAction(self, current_state, q_table_player):
+        for i in range(9):
+            if self.board[i//3][i%3] == '-': 
+                self.max_q_value_to_choose = q_table_player[current_state][i]
+                self.max_position_to_choose = i
+                break
+        for i in range(9):
+            if self.max_q_value_to_choose < q_table_player[current_state][i]:
+                if self.board[i//3][i%3] == '-': 
+                    self.max_q_value_to_choose = q_table_player[current_state][i]
+                    self.max_position_to_choose = i
+        return self.max_position_to_choose
+        
     def play(self, player):
         if player == 'O':
-            action = np.argmax(self.q_table_player_O[self.current_state])
+            action = self.chooseAction(self.current_state, self.q_table_player_O)
             current_q_value = self.q_table_player_O[self.current_state][action]
         else:
-            action = np.argmax(self.q_table_player_X[self.current_state])    
+            action = self.chooseAction(self.current_state, self.q_table_player_X)  
             current_q_value = self.q_table_player_X[self.current_state][action]
             
-        self.board[action] = player
+        self.board[action//3][action%3] = player
         next_q_state = self.convert_to_state()
         
         reward_X = 0
@@ -124,9 +144,59 @@ class TicTacToe:
             self.q_table_player_X[self.current_state] = new_q_value
             self.current_state = next_q_state
 
+    def start(self):
+        self.make_environment()
+        
+        player = 'X'
+        episodes = 200000
+        
+        for ep in range(episodes):
+            player = 'X'
+            print("Eps = {}".format(ep))
+            
+            while True:
+                self.play(player)
+                if self.is_winner('X') or self.is_winner('O') or self.is_draw():
+                    self.reset()
+                    break
+                player = self.swap_player(player)
 
-
+    def play_vs_human(self):
+        self.reset()
+        human = input("Choose your turn X/O: ")
+        if (human == 'X'):
+            self.computer = 'O'
+            self.computer_q_table = self.q_table_player_O
+        else: 
+            self.computer = 'X'
+            self.computer_q_table = self.q_table_player_X
+        
+        turn  = human
+        while True:
+            if turn == human:
+                action = int(input("Input position to fix the spot: "))
+                while self.board[action//3][action%3] != '-':
+                    print("The position has filled yet.")
+                    action = int(input("Input position to fix the spot: "))
+                self.board[action//3][action%3] = human
+                self.current_state = self.convert_to_state()
+            else:
+                action = self.chooseAction(self.current_state, self.computer_q_table)
+                self.board[action//3][action%3] = self.computer
+                self.current_state = self.convert_to_state()
+            
+            print(self.board)
+            if self.is_winner(human):
+                print("Human win!")
+                break
+            if self.is_winner(self.computer):
+                print("Computer win!")
+                break
+            if self.is_draw():
+                print("Draw!!!")
+                break
+            turn = self.swap_player(turn)
+        
 boardgame = TicTacToe()
-boardgame.make_environment()
-print(boardgame.convert_to_state())
-print(boardgame.board)
+boardgame.start()
+boardgame.play_vs_human()
